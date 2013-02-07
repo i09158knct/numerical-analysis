b55c3839b1b99fa2b145df9a9ac963d10d70f472
i09158knct <i09158knct@gmail.com>
2013-02-07 14:04:12 +0900

    add No.11 programs

---
+++ no11/r11-1.js
@@ -0,0 +1,65 @@
+;(function _main() {
+  function firstDerivative(x, y) {
+    return y;
+  }
+
+  function targetFunction(x) {
+    return Math.exp(x);
+  }
+
+  var
+    x0,
+    y0,
+    number,
+    xWidth,
+    domain,
+    trueValues,
+    approximations;
+
+
+  x0 = 0;
+  y0 = targetFunction(x0);
+  number = 5;
+  xWidth = 0.1;
+
+  domain = (function _buildDomain(initX, number, width) {
+    var
+      domain = [],
+      i;
+    for (i = 0; i < number; i++) {
+      domain[i] = initX + width * i;
+    }
+    return domain;
+  })(x0 + 0.1, number, xWidth);
+
+  trueValues = domain.map(function(x) {
+    return targetFunction(x);
+  });
+
+  approximations = domain.map(function(x, index) {
+    return domain.slice(0, index + 1).reduce(function(acc, x) {
+      return acc + xWidth * firstDerivative(x, acc);
+    }, y0);
+  });
+
+  (function _output() {
+    var i;
+
+    console.log([
+      "x",
+      "近似値",
+      "真値",
+      "誤差"
+    ].join("\t"));
+
+    for (i = 0; i < number; i++) {
+      console.log([
+        domain[i],
+        approximations[i],
+        trueValues[i],
+        Math.abs(approximations[i] - trueValues[i])
+      ].join("\t"));
+    }
+  })();
+
+})();
